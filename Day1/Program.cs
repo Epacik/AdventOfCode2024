70e27da1bd8df5616e16858d26f7afd89778205e
Epat <i.am@epat.xyz>
2024-12-01 13:51:03 +0100

    Dodaj pliki projektów.

---
+++ Day1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+var input = File.ReadAllText("input.txt");
+var leftList = new List<int>();
+var rightList = new List<int>();
+
+foreach (var line in input.Split('\n'))
+{
+    var nums = line.Split("   ");
+    if (nums.Length < 2)
+        continue;
+
+    leftList.Add(int.Parse(nums[0]));
+    rightList.Add(int.Parse(nums[1]));
+}
+
+leftList.Sort();
+rightList.Sort();
+
+var distances = new List<int>();
+for (int i = 0; i < Math.Min(leftList.Count, rightList.Count); i++)
+{
+    var leftNum = leftList[i];
+    var rightNum = rightList[i];
+    distances.Add(Math.Abs(leftNum - rightNum));
+}
+
+Console.WriteLine($"Result: {distances.Sum()}");
---
+++ Day1Part2/Program.cs
@@ -0,0 +1,23 @@
+﻿var input = File.ReadAllText("input.txt");
+var leftList = new List<int>();
+var rightList = new List<int>();
+
+foreach (var line in input.Split('\n'))
+{
+    var nums = line.Split("   ");
+    if (nums.Length < 2)
+        continue;
+
+    leftList.Add(int.Parse(nums[0]));
+    rightList.Add(int.Parse(nums[1]));
+}
+
+int score = 0;
+foreach (var left in leftList)
+{
+    var rightCount = rightList.Count(x => x == left);
+
+    score += (rightCount * left);
+}
+
+Console.WriteLine($"Score is {score}");
