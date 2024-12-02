a86ce51e7ece6f7cf22fbf7cf87ebda92a41b74b
Epat <i.am@epat.xyz>
2024-12-02 22:59:16 +0100

    Day 2

---
+++ Day2/Part1/Program.cs
@@ -0,0 +1,38 @@
+﻿var input = File.ReadAllText("input.txt").Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+int safeCount = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    var line = input[i];
+    var report = line
+        .Split(' ')
+        .Select(int.Parse)
+        .ToArray();
+
+    bool isDec = report[1] < report[0];
+    bool isInc = report[0] < report[1];
+    bool isErr = report[0] == report[1];
+
+    if (isErr)
+        continue;
+
+    for (int j = 0; j < report.Length - 1; j++)
+    {
+        if (isErr)
+            break;
+
+        var diff = Math.Abs(report[j] - report[j + 1]);
+
+        if ((isDec && report[j] <= report[j + 1]) || (isInc && report[j] >= report[j + 1]) || diff == 0 || diff > 3)
+        {
+            isErr = true;
+            break;
+        }
+    }
+
+    if (!isErr)
+        safeCount++;
+}
+
+Console.WriteLine($"Safe: {safeCount}");
---
+++ Day2/Part2/Program.cs
@@ -0,0 +1,72 @@
+﻿var input = File.ReadAllText("input.txt").Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+int safeCount = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    var currentCount = safeCount;
+    var line = input[i];
+    var report = line
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+        .Select(int.Parse)
+        .ToList();
+    
+    if (IsSafe(report))
+    {
+        safeCount++;
+        continue;
+    }
+    
+    Console.Write("");
+
+    var count = report.Count;
+
+    for (int j = 0; j < count; j++)
+    {
+        report = line
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .Select(int.Parse)
+            .ToList();
+
+        report.RemoveAt(j);
+
+        if (IsSafe(report))
+        {
+            safeCount++;
+            break;
+        }
+    }
+
+    if (safeCount == currentCount)
+        Console.WriteLine($"""
+            UNSAFE: [{line}]
+            """);
+}
+
+Console.WriteLine($"Safe: {safeCount}");
+
+bool IsSafe(List<int> report)
+{
+    bool isDec = report[1] < report[0];
+    bool isInc = report[0] < report[1];
+    bool isErr = report[0] == report[1];
+
+    if (isErr)
+        return false;
+
+    for (int j = 0; j < report.Count - 1; j++)
+    {
+        if (isErr)
+            break;
+
+        var diff = Math.Abs(report[j] - report[j + 1]);
+
+        if ((isDec && report[j] <= report[j + 1]) || (isInc && report[j] >= report[j + 1]) || diff == 0 || diff > 3)
+        {
+            isErr = true;
+            break;
+        }
+    }
+
+    return !isErr;
+}
