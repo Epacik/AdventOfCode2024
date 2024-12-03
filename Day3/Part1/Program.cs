9ef208a00a22306d90659af8d8b2966fc982b69f
Epat <i.am@epat.xyz>
2024-12-03 18:26:00 +0100

    Day 3

---
+++ Day3/Part1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllText("input.txt");
+var regex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
+
+MatchCollection? matches = regex.Matches(input);
+
+Console.WriteLine();
+var sum = 0;
+foreach (var match in matches as IEnumerable<Match>)
+{
+    if (match.Groups.Count != 3)
+        continue;
+
+    var x = int.Parse(match.Groups[1].Value);
+    var y = int.Parse(match.Groups[2].Value);
+    sum += x * y;
+}
+
+Console.WriteLine($"Sum: {sum}");
---
+++ Day3/Part2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllText("input.txt");
+var regex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don\'t\(\)");
+
+MatchCollection? matches = regex.Matches(input);
+
+var enabled = true;
+var sum = 0;
+
+foreach (var match in matches as IEnumerable<Match>)
+{
+    if (match.Value.Equals("don't()"))
+    {
+        enabled = false;
+        continue;
+    }
+    if (match.Value.Equals("do()"))
+    {
+        enabled = true;
+        continue;
+    }
+
+    if (!enabled)
+        continue;
+
+    if (match.Groups.Count != 3)
+        continue;
+
+    var x = int.Parse(match.Groups[1].Value);
+    var y = int.Parse(match.Groups[2].Value);
+    sum += x * y;
+}
+
+Console.WriteLine($"Sum: {sum}");
